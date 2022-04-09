1e4665906257b001600182570bb93df549ac14eb
Jack Latimer <jacklatimer03@gmail.com>
2022-04-09 10:33:53 +0100

    done(section): finished explore section

--- components/Home/Explore.js
+++ components/Home/Explore.js
@@ -3,25 +3,45 @@ export default function Explore() {
   return (
     <>
       <div className="mx-auto mb-60 lg:container">
-        <div>
-          <h2 className="ogg-regular mb-6 px-5 text-[3rem] leading-[3rem] text-[#2d2d2d] lg:mb-0 lg:px-0 lg:text-[4.1rem] lg:leading-[14rem]">
-            More to explore online
-          </h2>
+        <div className="flex items-center justify-between">
+          <div className="">
+            <h2 className="ogg-regular mb-6 pl-5 text-[3rem] leading-[3rem] text-[#2d2d2d] lg:mb-0 lg:pl-0 lg:text-[4.1rem] lg:leading-[14rem]">
+              More to explore online
+            </h2>
+          </div>
+          <div>
+            <a href="/" className="plain-regular text-md text-[#ec0426]">
+              See all
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="45px"
+                height="35px"
+                fill="#ec0426"
+                viewBox="0 0 29 32"
+                version="1.1"
+                className="inline-block"
+              >
+                <g id="surface1">
+                  <path d="M 22.488281 6.398438 C 21.9375 6.734375 20.484375 7.464844 19.871094 7.71875 C 17.703125 8.605469 15.613281 9.140625 13.246094 9.421875 C 12.148438 9.550781 11.300781 9.578125 10.683594 9.496094 C 10.085938 9.417969 9.503906 9.300781 8.816406 9.109375 C 8.519531 9.027344 8.277344 8.96875 8.273438 8.972656 C 8.253906 8.996094 11.246094 11.324219 13.542969 13.066406 C 14.164062 13.539062 14.679688 13.9375 14.6875 13.953125 C 14.695312 13.96875 14.4375 14.253906 14.109375 14.582031 C 13.78125 14.914062 12.101562 16.636719 10.371094 18.414062 L 7.222656 21.644531 L 7.660156 22.09375 L 8.101562 22.542969 L 11.820312 18.675781 C 13.871094 16.546875 15.550781 14.804688 15.5625 14.804688 C 15.570312 14.804688 15.851562 15.160156 16.1875 15.601562 C 17.539062 17.359375 18.671875 18.933594 19.609375 20.335938 L 20.171875 21.175781 L 20.171875 19.226562 C 20.171875 17.140625 20.199219 16.664062 20.390625 15.097656 C 20.730469 12.308594 21.597656 9.097656 22.679688 6.613281 C 22.777344 6.382812 22.859375 6.191406 22.851562 6.1875 C 22.847656 6.183594 22.683594 6.28125 22.488281 6.398438 Z M 22.488281 6.398438 " />
+                </g>
+              </svg>
+            </a>
+          </div>
         </div>
         <div className="grid grid-cols-1 px-5 lg:px-0">
-          <div className="flex flex-row border-t-2 border-[#c2bfba]">
-            <div className="basis-1/2 py-24">
+          <div className="flex flex-col border-t-2 border-[#c2bfba] lg:flex-row">
+            <div className="basis-1/2 pt-8 pb-2 lg:py-24">
               <h3 className="ogg-medium text-[2.6rem] leading-10 text-[#2d2d2d]">
                 COLLECTION AREAS
               </h3>
             </div>
-            <div className="ml-12 basis-1/2 py-24">
+            <div className="basis-1/2 py-4 lg:ml-12 lg:py-24">
               <p className="plain-regular max-w-[22rem] text-sm text-[#93928e]">
-                These cllection areas study, exhibit, and care for the objects
+                These collection areas study, exhibit, and care for the objects
                 in The Met collection.
               </p>
             </div>
-            <div className="py-24">
+            <div className="py-4 lg:py-24">
               <a href="/">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -38,19 +58,19 @@ export default function Explore() {
               </a>
             </div>
           </div>
-          <div className="flex flex-row border-t-2 border-[#c2bfba]">
-            <div className="basis-1/2 py-24">
+          <div className="flex flex-col border-t-2 border-[#c2bfba] lg:flex-row">
+            <div className="basis-1/2 pt-8 pb-2 lg:py-24">
               <h3 className="ogg-medium text-[2.6rem] leading-10 text-[#2d2d2d]">
                 CELEBRATING 150 YEARS
               </h3>
             </div>
-            <div className="ml-12 basis-1/2 py-24">
+            <div className="basis-1/2 py-4 lg:ml-12 lg:py-24">
               <p className="plain-regular max-w-[22rem] text-sm text-[#93928e]">
                 Honoring The Met&apos;s anniversary throughout 2020 with
                 exhibitions, events, and new ways to connect with art.
               </p>
             </div>
-            <div className="py-24">
+            <div className="py-4 lg:py-24">
               <a href="/">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -67,20 +87,20 @@ export default function Explore() {
               </a>
             </div>
           </div>
-          <div className="flex flex-row border-t-2 border-[#c2bfba]">
-            <div className="basis-1/2 py-24">
+          <div className="flex flex-col border-t-2 border-[#c2bfba] lg:flex-row">
+            <div className="basis-1/2 pt-8 pb-2 lg:py-24">
               <h3 className="ogg-medium text-[2.6rem] leading-10 text-[#2d2d2d]">
                 TRAVEL WITH THE MET
               </h3>
             </div>
-            <div className="ml-12 basis-1/2 py-24">
+            <div className="basis-1/2 py-4 lg:ml-12 lg:py-24">
               <p className="plain-regular max-w-[22rem] text-sm text-[#93928e]">
                 Our trips offer travelers the unique opportunity to see the
                 world and humanity&apos;s highest artistic achievements with the
                 guidance of The Met&apos;s renowed curators and educators.
               </p>
             </div>
-            <div className="py-24">
+            <div className="py-4 lg:py-24">
               <a href="/">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -97,19 +117,19 @@ export default function Explore() {
               </a>
             </div>
           </div>
-          <div className="flex flex-row border-t-2 border-b-2 border-[#c2bfba]">
-            <div className="basis-1/2 py-24">
+          <div className="flex flex-col border-t-2 border-b-2 border-[#c2bfba] lg:flex-row">
+            <div className="basis-1/2 pt-8 pb-2 lg:py-24">
               <h3 className="ogg-medium text-[2.6rem] leading-10 text-[#2d2d2d]">
                 VIRTUAL EVENTS
               </h3>
             </div>
-            <div className="ml-12 basis-1/2 py-24">
+            <div className="basis-1/2 py-4 lg:ml-12 lg:py-24">
               <p className="plain-regular max-w-[22rem] text-sm text-[#93928e]">
                 Engage with art and culture remotely with a variety of virtual
                 events for all audiences.
               </p>
             </div>
-            <div className="py-24">
+            <div className="py-4 lg:py-24">
               <a href="/">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
